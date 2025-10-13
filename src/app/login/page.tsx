04115671b3d3a984c7da6f4966bec6437138824e
SAKTHI KUMAR S <adolfsakthi@gmail.com>
2025-10-13 23:19:13 +0000

    see this out put how much scrolling its coming

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default function LoginPage() {
     return (
-      <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2 xl:min-h-screen">
+      <div className="w-full lg:grid lg:min-h-screen lg:grid-cols-2">
         <div className="hidden bg-muted lg:block">
           <Image
             src="https://picsum.photos/seed/building-night/1200/1800"
@@ -17,7 +17,7 @@ export default function LoginPage() {
             priority
           />
         </div>
-        <div className="flex items-center justify-center py-12 bg-accent">
+        <div className="flex items-center justify-center bg-accent">
           <div className="mx-auto grid w-[350px] gap-6">
             <div className="grid gap-2 text-center">
                 <div className="mb-8 flex flex-col items-center text-center">
